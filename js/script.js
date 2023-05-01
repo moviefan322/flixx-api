11dec610cc6867fa317c4267bbc16757b816b5a3
moviefan322 <philman202@yahoo.com>
2023-05-01 14:32:46 -0400

    adds search functionality

--- js/script.js
+++ js/script.js
@@ -3,13 +3,23 @@ const popularMoviesEl = document.querySelector("#popular-movies");
 
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: "",
+    type: "",
+    page: 1,
+    totalPages: 1,
+  },
+  api: {
+    apiKey: "1c17ca0eb6a609b26ef677e0da002e09",
+    apiUrl: "https://api.themoviedb.org/3/",
+  },
 };
 
 // Fetch Data
 async function fetchAPIData(endpoint) {
   showSpinner();
-  const API_KEY = "1c17ca0eb6a609b26ef677e0da002e09";
-  const API_URL = "https://api.themoviedb.org/3/";
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
@@ -21,6 +31,86 @@ async function fetchAPIData(endpoint) {
 
   hideSpinner();
   return data;
+}
+
+async function searchAPIData() {
+  showSpinner();
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+  );
+
+  const data = await response.json();
+
+  console.log("fetchdata", data);
+
+  hideSpinner();
+  return data;
+}
+
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  global.search.type = urlParams.get("type");
+  global.search.term = urlParams.get("search-term");
+
+  if (global.search.term !== "" && global.search.term !== null) {
+    const { results, total_pages, page } = await searchAPIData();
+
+    if (results.length === 0) {
+      showAlert("No results found");
+      return;
+    }
+
+    displaySearchResults(results);
+
+    document.querySelector("#search-term").value = "";
+    console.log(results);
+  } else {
+    showAlert("Please enter a search term");
+  }
+}
+
+function displaySearchResults(results) {
+  results.forEach((result) => {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("card");
+    newDiv.innerHTML = `
+    ${
+      result.poster_path
+        ? `
+        <a href="movie-details.html?id=${result.id}">
+        <img
+      src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+      class="card-img-top"
+      alt="${result.title ? result.title : result.name}"
+    /></a>`
+        : `            
+        <a href="movie-details.html?id=${result.id}">
+        <img
+    src="images/no-image.jpg"
+    class="card-img-top"
+    alt="${result.title ? result.title : result.name}"
+  />
+  </a>`
+    }
+        <div class="card-body">
+          <h5 class="card-title">${
+            result.title ? result.title : result.name
+          }</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${
+              result.release_date ? result.release_date : result.first_air_date
+            }</small>
+          </p>
+        </div>
+        `;
+
+    document.querySelector("#search-results").appendChild(newDiv);
+  });
 }
 
 function showSpinner() {
@@ -209,7 +299,7 @@ alt="${response.name}"
    <h2>${response.name}</h2>
    <p>
      <i class="fas fa-star text-primary"></i>
-     ${response.vote_average} / 10
+     ${response.vote_average.toFixed(1)} / 10
    </p>
    <p class="text-muted">Release Date: ${response.first_air_date}</p>
    <p>
@@ -246,6 +336,54 @@ alt="${response.name}"
   document.querySelector("#show-details").appendChild(newDiv);
 }
 
+async function displaySlider() {
+  const { results } = await fetchAPIData("movie/now_playing");
+  console.log("nowp", results);
+
+  results.forEach((movie) => {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("swiper-slide");
+    newDiv.innerHTML = `         
+    <div class="swiper-slide">
+    <a href="movie-details.html?id=${movie.id}">
+      <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" />
+    </a>
+    <h4 class="swiper-rating">
+      <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+        1
+      )} / 10
+    </h4>
+  </div>`;
+    document.querySelector(".swiper-wrapper").appendChild(newDiv);
+
+    initSwiper();
+  });
+}
+
+function initSwiper() {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 // Display Backdrop On Details Pages
 function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement("div");
@@ -278,6 +416,15 @@ const highlightActiveLink = () => {
   });
 };
 
+function showAlert(message, className = "error") {
+  const alertEl = document.createElement("div");
+  alertEl.classList.add("alert", className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector("#alert").appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+}
+
 function addCommasToNumber(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -288,6 +435,7 @@ function init() {
     case "/":
     case "/index.html":
       renderPopularmovies();
+      displaySlider();
       break;
     case "/shows.html":
       renderPopularTVShows();
@@ -299,8 +447,7 @@ function init() {
       displayShowDetails();
       break;
     case "/search.html":
-      console.log("Search");
-      ``;
+      search();
       break;
   }
 
