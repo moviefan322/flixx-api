ef0ce7b1cb71d8fee33d6172a5f3452cd67a879c
moviefan322 <philman202@yahoo.com>
2023-05-01 12:17:54 -0400

    movie data is loading

--- js/script.js
+++ js/script.js
@@ -60,10 +60,10 @@ function renderMovie(data) {
     </div>
   </div>`;
 
-  console.log(popularMoviesEl);
   popularMoviesEl.appendChild(card);
 }
 
+// Renders top 20 popular movies
 async function renderPopularmovies() {
   const { results } = await fetchAPIData("movie/popular");
 
@@ -74,11 +74,11 @@ async function renderPopularmovies() {
   });
 }
 
+//Renders top 20 popular TVshows
 async function renderPopularTVShows() {
   const { results } = await fetchAPIData("tv/popular");
 
   const popTV = results;
-  console.log("TV", popTV);
 
   popTV.forEach((show) => {
     const newDiv = document.createElement("div");
@@ -110,6 +110,71 @@ async function renderPopularTVShows() {
   });
 }
 
+//Display single movie details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split("=")[1];
+  const response = await fetchAPIData(`movie/${movieId}`);
+  console.log(response);
+  const newDiv = document.createElement("div");
+  newDiv.innerHTML = `<div class="details-top">
+  <div>
+  ${
+    response.poster_path
+      ? `    <img
+src="https://image.tmdb.org/t/p/w500/${response.poster_path}"
+class="card-img-top"
+alt="${response.title}"
+/>`
+      : `    <img
+src="images/no-image.jpg"
+class="card-img-top"
+alt="${response.title}"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${response.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      8 / 10
+    </p>
+    <p class="text-muted">Release Date: ${response.release_date}</p>
+    <p>
+      ${response.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      <li>${response.genres[0].name}</li>
+      <li>${response.genres[1].name}</li>
+      <li>${response.genres[2].name}</li>
+    </ul>
+    <a href="${
+      response.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${response.budget}</li>
+    <li><span class="text-secondary">Revenue:</span> $${response.revenue}</li>
+    <li><span class="text-secondary">Runtime:</span> ${
+      response.runtime
+    } minutes</li>
+    <li><span class="text-secondary">Status:</span> ${response.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group"> ${response.production_companies
+    .map((company) => {
+      return `<li>${company.name}</li>`;
+    })
+    .join("")}</div>
+</div>`;
+
+  document.querySelector("#movie-details").appendChild(newDiv);
+  console.log(response.production_companies);
+}
+
 //Highlight active link
 const highlightActiveLink = () => {
   const links = document.querySelectorAll(".nav-link");
@@ -126,14 +191,12 @@ function init() {
     case "/":
     case "/index.html":
       renderPopularmovies();
-      console.log("Home");
       break;
     case "/shows.html":
       renderPopularTVShows();
-      console.log("Shows");
       break;
     case "/movie-details.html":
-      console.log("movie details");
+      displayMovieDetails();
       break;
     case "/tv-details.html":
       console.log("TV Details");
